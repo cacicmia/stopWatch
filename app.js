fad1d3623b428ee285be157ee0ba7b703c1ee25b
Mia Ćaćić <cacic.miaa@gmail.com>
2018-09-07 09:43:53 +0200

    Add button functionality. Still unstoppable

--- app.js
+++ app.js
@@ -4,23 +4,35 @@ let start = false;
 
 document.getElementById('no1').addEventListener('click', watchControl);
 
-document.getElementById('no2').addEventListener('click', function(){
-});
+document.getElementById('no2').addEventListener('click', reset);
 
-document.getElementById('no2').addEventListener('click', function(){
-});
+document.getElementById('no3').addEventListener('click', record);
 
 
-document.addEventListener('keypress', function(){
+document.addEventListener('keydown', function(event){
+  switch(event.which) {
+    case 82:
+      reset();
+      break;
+    case 83:
+      watchControl();
+      break;
+    case 84:
+      record();
+      break;
+    default:
+      break;
+  }
 });
 function watchControl(){
   if (start == false) {
     start = true;
-  var watch = setInterval(count, 10);
+  var interval = setInterval(count, 10);
 
 } else {
   start = false;
-  clearInterval(watch);
+  clearInterval(interval);
+
 }
 
 };
@@ -32,4 +44,13 @@ function count(){
   }
 
   document.getElementById('count').innerHTML = `${sec}.${msec}`;
+}
+function reset() {
+  location.reload();
+}
+function record() {
+let rTime = document.createElement('p');
+rTime.innerHTML = `${sec}.${msec}`;
+  document.getElementById('PT').appendChild(rTime);
+
 }
